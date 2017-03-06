491e7eb6db9e476826e79f0e17d555d76cdea3dd
litheon <litheon@gmail.com>
2017-03-06 02:09:54 +0100

    trying to fix tabs in license banner

--- src/stereo_matcher_birchfield_tomasi.cpp
+++ src/stereo_matcher_birchfield_tomasi.cpp
@@ -1,21 +1,21 @@
-/* Copyright 2017 Toon Van den Zegel. All Rights Reserved.								*/
-/*																						*/
-/* This file is part of fast_bilateral_space_stereo.									*/
-/*																						*/
-/* fast_bilateral_space_stereo is free software :										*/
-/* you can redistribute it and / or modify												*/
-/* it under the terms of the GNU General Public License as published by					*/
-/* the Free Software Foundation, either version 3 of the License, or					*/
-/* (at your option) any later version.													*/
-/*																						*/
-/* fast_bilateral_space_stereo is distributed in the hope that it will be useful,		*/
-/* but WITHOUT ANY WARRANTY; without even the implied warranty of						*/
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the							*/
-/* GNU General Public License for more details.											*/
-/*																						*/
-/* You should have received a copy of the GNU General Public License					*/
-/* along with fast_bilateral_space_stereo.												*/
-/* If not, see <http://www.gnu.org/licenses/>.											*/
+/* Copyright 2017 Toon Van den Zegel. All Rights Reserved.                              */
+/*                                                                                      */
+/* This file is part of fast_bilateral_space_stereo.                                    */
+/*                                                                                      */
+/* fast_bilateral_space_stereo is free software :                                       */
+/* you can redistribute it and / or modify                                              */
+/* it under the terms of the GNU General Public License as published by                 */
+/* the Free Software Foundation, either version 3 of the License, or                    */
+/* (at your option) any later version.                                                  */
+/*                                                                                      */
+/* fast_bilateral_space_stereo is distributed in the hope that it will be useful,       */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of                       */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the                          */
+/* GNU General Public License for more details.                                         */
+/*                                                                                      */
+/* You should have received a copy of the GNU General Public License                    */
+/* along with fast_bilateral_space_stereo.                                              */
+/* If not, see <http://www.gnu.org/licenses/>.                                          */
 
 #include "stereo_matcher_birchfield_tomasi.h"
 
